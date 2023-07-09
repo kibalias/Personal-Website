aaa14327d594c01b3229b903dccfd84b7e7a8db3
kibalias <74294078+kibalias@users.noreply.github.com>
2023-07-09 18:22:19 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.{html,js}",
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'palette': {
+          100: '#FF942F',
+          200: '#EEA25A',
+          300: '#FFBB0C',
+          400: '#964E0B',
+          500: '#6B3706',
+          600: '#383838'
+        }
+      }, 
+      fontFamily: {
+        'Poppins': ['Poppins'],
+        'Lilita': ['Lilita One']
+        },
+    },
+  },
+  plugins: [],
+}
