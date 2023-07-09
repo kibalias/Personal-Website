c5a2ce0c1ad95b0e4e73c234d9182389c3b570e9
kibalias <74294078+kibalias@users.noreply.github.com>
2023-07-10 00:27:31 +0800

    mobile view fixes

--- src/js/load-pages.js
+++ src/js/load-pages.js
@@ -10,3 +10,19 @@ $(function(){
 $(function(){
     $(".contact-placeholder").load('src/templates/contact.html'); 
 });
+
+
+// open mobile menu once menu bar is clicked/tapped
+var mobile_menu = document.getElementById('menu-btn');
+var close_menu = document.getElementById('close-btn');
+var mobile_menu_placeholder = document.getElementById('mobile-menu');
+
+mobile_menu.addEventListener('click', function(){
+    mobile_menu_placeholder.classList.remove('hidden');
+    mobile_menu_placeholder.classList.add('absolute');
+});
+
+close_menu.addEventListener('click', function(){
+    mobile_menu_placeholder.classList.add('hidden');
+    mobile_menu_placeholder.classList.remove('absolute');
+});
