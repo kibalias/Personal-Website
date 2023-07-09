c9bfe8a07cc26570240e2b09c2f289ae3c8a597e
kibalias <74294078+kibalias@users.noreply.github.com>
2023-07-09 21:21:55 +0800

    loading html pages w ajax

---
+++ src/js/load-pages.js
@@ -0,0 +1,12 @@
+// Ajax functions to load html files inside the div in index.html
+$(function(){
+    $(".about-placeholder").load('src/templates/about.html'); 
+});
+
+$(function(){
+    $(".portfolio-placeholder").load('src/templates/portfolio.html'); 
+});
+
+$(function(){
+    $(".contact-placeholder").load('src/templates/contact.html'); 
+});
---
+++ src/js/open-pdf.js
@@ -0,0 +1,5 @@
+var pdf_btn = document.getElementById('resume-download-btn');
+
+pdf_btn.addEventListener('click', function(){
+    window.open('assets/pdf/dummy.pdf', '_blank');
+})
