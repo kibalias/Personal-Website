1e603065d1de2768cc08fabefa235960ad3f1167
kibalias <74294078+kibalias@users.noreply.github.com>
2023-11-23 14:53:49 +0800

    animate hamburger menu

--- src/js/load-pages.js
+++ src/js/load-pages.js
@@ -11,22 +11,6 @@ $(function(){
     $(".contact-placeholder").load('src/templates/contact.html'); 
 });
 
-
-// open mobile menu once menu bar is clicked/tapped
-var mobile_menu = document.getElementById('menu-btn');
-var close_menu = document.getElementById('close-btn');
-var mobile_menu_placeholder = document.getElementById('mobile-menu');
-
-mobile_menu.addEventListener('click', function(){
-    mobile_menu_placeholder.classList.remove('hidden');
-    mobile_menu_placeholder.classList.add('absolute');
-});
-
-close_menu.addEventListener('click', function(){
-    mobile_menu_placeholder.classList.add('hidden');
-    mobile_menu_placeholder.classList.remove('absolute');
-});
-
 //load portfolio pages
 function loadWork(val){
     console.log(val);
@@ -44,11 +28,6 @@ function loadWork(val){
     catch {
         alert('Non-existing or cannot locate page!');
     }
-    /*if(val == 1){
-        window.open('src/templates/project/mafumafu-interface-design.html');
-    } else {
-        window.open('src/templates/misc/under-construction.html');
-    }*/
 }
 
 //redirect to github repository
